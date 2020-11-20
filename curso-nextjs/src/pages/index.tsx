87f4683b1f203985f4f09ff347e3b5752b8471ab
victorgcampanella <vgcgalvao@outlook.com>
2020-11-20 11:33:25 -0300

    removendo arquivos para aplicar o prismic

--- curso-nextjs/src/pages/index.tsx
+++ curso-nextjs/src/pages/index.tsx
@@ -12,13 +12,6 @@ interface HomeProps {
 }
 
 export default function Home({recommendedProducts}: HomeProps) {
-  async function handleSum() {
-    console.log(process.env.NEXT_PUBLIC_API_URL)
-    const math = (await import('../lib/math')).default;
-
-    alert(math.sum(3, 5));
-  }
-
   return (
     <div>
       <section>
@@ -40,8 +33,6 @@ export default function Home({recommendedProducts}: HomeProps) {
           })}
         </ul>
       </section>
-
-      <button onClick={handleSum}>Sum!</button>
     </div>
   )
 }
